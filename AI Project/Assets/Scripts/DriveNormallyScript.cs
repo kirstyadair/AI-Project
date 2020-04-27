f7f359c9b703641c86721e0bb4d48a84bcd5e6ab
Kirsty Adair <kirstyaadair@gmail.com>
2020-04-27 16:24:48 +0100

    Car and basic road

---
+++ AI Project/Assets/Scripts/DriveNormallyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveNormallyScript : MonoBehaviour
+{
+    [SerializeField] GameObject car;
+    GameObject currentRoadPiece;
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Road")) currentRoadPiece = collision.gameObject;
+    }
+}
---
+++ AI Project/Assets/Scripts/RoadPointsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPointsScript : MonoBehaviour
+{
+    [SerializeField] GameObject leftLanePoints;
+    [SerializeField] GameObject rightLanePoints;
+
+    public Transform[] GetPoints(bool isLeftLane)
+    {
+        if (isLeftLane)
+        {
+            return leftLanePoints.GetComponentsInChildren<Transform>();
+        }
+        else
+        {
+            return rightLanePoints.GetComponentsInChildren<Transform>();
+        }
+    }
+}
